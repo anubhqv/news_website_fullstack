7b2d4bca0b243e29d93c27f946ac75e92bf2811e
kulbhooshan <91148852+anubhqv@users.noreply.github.com>
2025-04-14 13:52:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config(); // 👈 this line must come first
+require('./config/db');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const UserRouter = require('./api/User');
+app.use('/user', UserRouter);
+
+app.listen(port, () => {
+    console.log('Server running on port', port);
+});
